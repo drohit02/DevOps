1db09101fbf0fdd0cc065502068593d5273583ca
drohit02 <drohit045@gmail.com>
2024-09-10 23:46:26 +0530

    python script started

---
+++ Python/EvenOdd.py
@@ -0,0 +1,6 @@
+#Determine given number is even or odd
+number = 37
+if number % 2 == 0 :
+    print(number," is even")
+else :
+    print(number," is odd")
---
+++ Python/HelloWorld.py
@@ -0,0 +1,6 @@
+#print hello world
+
+welcome = "Hello,World!!s"
+print(welcome)
+#print hello world
+print("Hello,World!!")
